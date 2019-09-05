c830646043adbcca15ea64948e3b3ed7a0e32b1f
Teebo <ngubanethabo.ambrose@gmail.com>
2019-09-05 08:29:38 +0200

    WIP

--- src/redux/movie-list/action-creators.ts
+++ src/redux/movie-list/action-creators.ts
@@ -1,8 +1,8 @@
 import { ADD_MOVIE } from './action-types';
 
 export const addMovie = (payload) => {
-   type: ADD_MOVIE,
-   payload: {
-     
-   }
+  return {
+    type: ADD_MOVIE,
+    payload
+  }
 }
---
+++ src/redux/reducers/movie-list.ts
@@ -0,0 +1,13 @@
+import { ADD_MOVIE } from '../movie-list/action-types';
+
+const initialState = {
+  allMovieIds: [],
+  moviesMapById: {}
+};
+
+export default (state = initialState, action) => {
+  switch (action.type) {
+    case ADD_MOVIE:
+
+  }
+}
